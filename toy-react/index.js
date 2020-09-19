eb84c2f40993462e79cebe82505e4abffc8e8749
kyrielin <hq.lin@foxmail.com>
2020-09-19 20:14:32 +0800

    four

--- toy-react/index.js
+++ toy-react/index.js
@@ -126,6 +126,8 @@ class Game extends Component {
 // ========================================
 
 render(<Game />, document.getElementById("root"));
+// const game = <Game />;
+// console.log(game.vdom);
 
 function calculateWinner(squares) {
   const lines = [
--- toy-react/toy-react.js
+++ toy-react/toy-react.js
@@ -1,37 +1,140 @@
 const RENDER_TO_DOM = Symbol('render to dom');
 
-class ElementWrapper {
-  constructor(type) {
-    this.root = document.createElement(type);
+export class Component {
+  constructor() {
+    this.props = Object.create(null);
+    this.children = [];
+    this._range = null;
   }
+
   setAttribute(name, value) {
-    if (name.match(/^on([\s\S]+)$/)) {
-      this.root.addEventListener(RegExp.$1.replace(/^[\s\S]+/, c => c.toLowerCase()), value);
-    } else {
-      if (name === 'className') {
-        this.root.setAttribute('class', value)
-      } else {
-        this.root.setAttribute(name, value)
-      }
-    }
+    this.props[name] = value;
   }
   appendChild(component) {
-    const range = document.createRange();
-    range.setStart(this.root, this.root.childNodes.length);
-    range.setEnd(this.root, this.root.childNodes.length);
-    component[RENDER_TO_DOM](range);
+    this.children.push(component);
+  }
+
+  get vdom() {
+    return this.render().vdom;;
+  }
+
+  get vchildren() {
+    return this.children.map(child => child.vdom);
+  }
+
+  [RENDER_TO_DOM](range) {
+    this._range = range;
+    this.render()[RENDER_TO_DOM](range);
+  }
+
+  rerender() {
+    let oldRange = this._range;
+
+    let range = document.createRange();
+    range.setStart(this._range.startContainer, this._range.startOffset);
+    range.setEnd(this._range.startContainer, this._range.startOffset);
+    this[RENDER_TO_DOM](range);
+
+    oldRange.setStart(range.endContainer, range.endOffset);
+    oldRange.deleteContents();
+  }
+
+  setState(newState) {
+    if (this.state === null || typeof this.state !== 'object') {
+      this.state = newState;
+      this.rerender();
+      return;
+    }
+    const merge = (oldState, newState) => {
+      for (let p in newState) {
+        if (oldState[p] === null || typeof oldState[p] !== 'object') {
+          oldState[p] = newState[p];
+        } else {
+          merge(oldState[p], newState[p]);
+        }
+      }
+    }
+    merge(this.state, newState);
+    this.rerender();
+  }
+}
+
+class ElementWrapper extends Component {
+  constructor(type) {
+    super(type);
+    this.type = type;
+  }
+  // setAttribute(name, value) {
+  //   if (name.match(/^on([\s\S]+)$/)) {
+  //     this.root.addEventListener(RegExp.$1.replace(/^[\s\S]+/, c => c.toLowerCase()), value);
+  //   } else {
+  //     if (name === 'className') {
+  //       this.root.setAttribute('class', value)
+  //     } else {
+  //       this.root.setAttribute(name, value)
+  //     }
+  //   }
+  // }
+  // appendChild(component) {
+  //   const range = document.createRange();
+  //   range.setStart(this.root, this.root.childNodes.length);
+  //   range.setEnd(this.root, this.root.childNodes.length);
+  //   component[RENDER_TO_DOM](range);
+  // }
+
+  get vdom() {
+    return this;
+    // {
+    //   type: this.type,
+    //   props: this.props,
+    //   children: this.children.map(child => child.vdom),
+    // }
   }
 
   [RENDER_TO_DOM](range) {
     range.deleteContents();
-    range.insertNode(this.root);
+
+    const root = document.createElement(this.type);
+    for (let name in this.props) {
+        const value = this.props[name];
+        if (name.match(/^on([\s\S]+)$/)) {
+          root.addEventListener(RegExp.$1.replace(/^[\s\S]+/, c => c.toLowerCase()), value);
+        } else {
+          if (name === 'className') {
+            root.setAttribute('class', value)
+          } else {
+            root.setAttribute(name, value)
+          }
+        }
+    }
+
+    for (let child of this.children) {
+      const childRange = document.createRange();
+      childRange.setStart(root, root.childNodes.length);
+      childRange.setEnd(root, root.childNodes.length);
+      child[RENDER_TO_DOM](childRange);
+    }
+
+    range.insertNode(root);
   }
 }
 
-class TextWrapper {
+class TextWrapper extends Component {
   constructor(content) {
+    super(content);
+    this.type = '#text';
+    this.content = content;
     this.root = document.createTextNode(content)
   }
+
+  get vdom() {
+    return this;
+    // {
+    //   type: '#text',
+    //   content: this.content,
+    // }
+  }
+
   [RENDER_TO_DOM](range) {
     range.deleteContents();
     range.insertNode(this.root);
@@ -70,55 +173,4 @@ export function render(component, parentElement) {
   range.setStart(parentElement, 0);
   range.setEnd(parentElement, parentElement.childNodes.length);
   component[RENDER_TO_DOM](range);
-}
-
-export class Component {
-  constructor() {
-    this.props = Object.create(null);
-    this.children = [];
-    this._range = null;
-  }
-
-  setAttribute(name, value) {
-    this.props[name] = value;
-  }
-  appendChild(component) {
-    this.children.push(component);
-  }
-
-  [RENDER_TO_DOM](range) {
-    this._range = range;
-    this.render()[RENDER_TO_DOM](range);
-  }
-
-  rerender() {
-    let oldRange = this._range;
-
-    let range = document.createRange();
-    range.setStart(this._range.startContainer, this._range.startOffset);
-    range.setEnd(this._range.startContainer, this._range.startOffset);
-    this[RENDER_TO_DOM](range);
-
-    oldRange.setStart(range.endContainer, range.endOffset);
-    oldRange.deleteContents();
-  }
-
-  setState(newState) {
-    if (this.state === null || typeof this.state !== 'object') {
-      this.state = newState;
-      this.rerender();
-      return;
-    }
-    const merge = (oldState, newState) => {
-      for (let p in newState) {
-        if (oldState[p] === null || typeof oldState[p] !== 'object') {
-          oldState[p] = newState[p];
-        } else {
-          merge(oldState[p], newState[p]);
-        }
-      }
-    }
-    merge(this.state, newState);
-    this.rerender();
-  }
 }
